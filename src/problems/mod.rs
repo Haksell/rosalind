3c1ff7af96ed6676c1f9e7fac5293faaa8c2156c
Axel Brisse <axel.brisse@gmail.com>
2024-10-10 05:23:14 +0200

    .

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -12,13 +12,13 @@ use std::collections::HashMap;
 
 lazy_static! {
     pub static ref PROBLEMS: HashMap<String, fn() -> String> = HashMap::from([
-        (("dna".to_string(), dna::subject as fn() -> String)),
-        (("fib".to_string(), fib::subject as fn() -> String)),
-        (("gc".to_string(), gc::subject as fn() -> String)),
-        (("hamm".to_string(), hamm::subject as fn() -> String)),
-        (("iprb".to_string(), iprb::subject as fn() -> String)),
-        (("prot".to_string(), prot::subject as fn() -> String)),
-        (("revc".to_string(), revc::subject as fn() -> String)),
-        (("rna".to_string(), rna::subject as fn() -> String)),
+        ("dna".to_string(), dna::subject as fn() -> String),
+        ("fib".to_string(), fib::subject as fn() -> String),
+        ("gc".to_string(), gc::subject as fn() -> String),
+        ("hamm".to_string(), hamm::subject as fn() -> String),
+        ("iprb".to_string(), iprb::subject as fn() -> String),
+        ("prot".to_string(), prot::subject as fn() -> String),
+        ("revc".to_string(), revc::subject as fn() -> String),
+        ("rna".to_string(), rna::subject as fn() -> String),
     ]);
 }
